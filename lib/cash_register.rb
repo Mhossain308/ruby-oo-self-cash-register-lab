41c6787414d15636d0c2d9c3d9bed73bcd383a6b
Mohamed Hossain <mz.hossain212@gmail.com>
2021-02-06 17:46:18 -0500

    commit tree 22b57fe116da4d5298671845e379eb0593a124a2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  attr_accessor :discount, :total, :items, :void_last_transaction
+
+  def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+        
+    
+    def add_item(title, price, quantity=1)
+        
+    end
+
+
+
+        
+end
+
 
